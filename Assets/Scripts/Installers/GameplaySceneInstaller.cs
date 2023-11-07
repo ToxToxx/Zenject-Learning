dd9dfae18568dc6c9ff95d83cd121cbfc8baa754
ToxToxx <107263231+ToxToxx@users.noreply.github.com>
2023-11-07 21:01:50 +0300

    made player spawn at spawn position

--- Assets/Scripts/Installers/GameplaySceneInstaller.cs
+++ Assets/Scripts/Installers/GameplaySceneInstaller.cs
@@ -3,10 +3,16 @@ using Zenject;
 
 public class GameplaySceneInstaller : MonoInstaller
 {
+    [SerializeField] private Player _playerPrefab;
+    [SerializeField] private Transform _playerSpawnPoint;
+
+
     public override void InstallBindings()
     {
-       
-        Container.Bind<MovementHandler>().FromNew().AsSingle().NonLazy();
+        Player player = Container.InstantiatePrefabForComponent<Player>(_playerPrefab, _playerSpawnPoint.position, Quaternion.identity, null);
+        Container.BindInterfacesAndSelfTo<Player>().FromInstance(player).AsSingle();
+
+        Container.Bind<MovementHandler>().AsSingle().NonLazy();
     }
 
     
